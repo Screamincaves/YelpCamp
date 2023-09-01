a583b44353e4ed49ae6ceab94b79b8ec6682d05b
Screamincaves <61538874+Screamincaves@users.noreply.github.com>
2023-09-01 15:31:40 +0100

    Lesson 778-779
    Added reviews form and validation

--- models/review.js
+++ models/review.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
-const Schema = moongoose.schema;
+const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
   body: String,
   rating: Number,
 });
-
-
 
 module.exports = mongoose.model("Review", reviewSchema);
