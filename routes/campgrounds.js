b5434beafec1f3a205e3b57c7766eeabd7dad30e
Screamincaves <61538874+Screamincaves@users.noreply.github.com>
2023-10-06 15:39:07 +0100

    Lesson 525
    Added isLoggedIn middleware

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,6 +4,7 @@ const catchAsync = require("../utils/catchAsync");
 const ExpressError = require("../utils/ExpressError");
 const Campground = require("../models/campground"); //require the campgrounds model created.
 const { campgroundSchema, reviewSchema } = require("../schemas.js");
+const { isLoggedIn } = require("../middleware");
 
 const validateCampground = (req, res, next) => {
   const { error } = campgroundSchema.validate(req.body);
@@ -20,12 +21,13 @@ router.get("/", async (req, res) => {
   res.render("campgrounds/index", { campgrounds });
 });
 
-router.get("/new", (req, res) => {
-  res.render("campgrounds/new");
+router.get("/new", isLoggedIn, (req, res) => {
+  return res.render("campgrounds/new");
 });
 
 router.post(
   "/",
+  isLoggedIn,
   validateCampground,
   catchAsync(async (req, res, next) => {
     //if (!req.body.campground) throw new ExpressError("invalid campground data", 400);
@@ -52,6 +54,7 @@ router.get(
 
 router.get(
   "/:id/edit",
+  isLoggedIn,
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     if (!campground) {
@@ -64,6 +67,7 @@ router.get(
 
 router.put(
   "/:id",
+  isLoggedIn,
   validateCampground,
   catchAsync(async (req, res) => {
     const { id } = req.params;
@@ -77,6 +81,7 @@ router.put(
 
 router.delete(
   "/:id",
+  isLoggedIn,
   catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
