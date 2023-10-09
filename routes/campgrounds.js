a0e3b2756a33de972d3cac57c62ca69e0db0febd
Screamincaves <61538874+Screamincaves@users.noreply.github.com>
2023-10-09 16:13:24 +0100

    Lesson 531 + 532
    Added author to campground and basic auth to the edit and delete buttons

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -32,6 +32,7 @@ router.post(
   catchAsync(async (req, res, next) => {
     //if (!req.body.campground) throw new ExpressError("invalid campground data", 400);
     const campground = new Campground(req.body.campground);
+    campground.author = req.user._id;
     await campground.save();
     req.flash("success", "Succesfully made a new campground");
     res.redirect(`/campgrounds/${campground._id}`);
@@ -41,9 +42,10 @@ router.post(
 router.get(
   "/:id",
   catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate(
-      "reviews"
-    );
+    const campground = await Campground.findById(req.params.id)
+      .populate("reviews")
+      .populate("author");
+    console.log(campground);
     if (!campground) {
       req.flash("error", "Campground not found");
       return res.redirect("/campgrounds");
--- seeds/index.js
+++ seeds/index.js
@@ -26,6 +26,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      author: "6523e12469df4482fa6e8b1e",
       location: `${cities[random1000].city}, ${cities[random1000].state} `,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: "https://source.unsplash.com/collection/483251",
