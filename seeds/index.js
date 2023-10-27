e5a9ee6c61ef6ef7aa82d250159ce10b29a6fa2f
Screamincaves <61538874+Screamincaves@users.noreply.github.com>
2023-10-27 14:58:31 +0100

    Lesson 550
    Added ability to add photos when editing a campground

--- seeds/index.js
+++ seeds/index.js
@@ -29,10 +29,23 @@ const seedDB = async () => {
       author: "6523e12469df4482fa6e8b1e",
       location: `${cities[random1000].city}, ${cities[random1000].state} `,
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: "https://source.unsplash.com/collection/483251",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad aliquid maxime consectetur repudiandae minima sequi voluptates, beatae commodi a aliquam eaque repellendus. Minus ratione unde quod corrupti deserunt accusamus cupiditate.",
       price,
+      images: [
+        {
+          url: "https://res.cloudinary.com/dkpywucqx/image/upload/v1698413830/YelpCamp/jrrsjemdvdkilztkotzi.jpg",
+          filename: "YelpCamp/jrrsjemdvdkilztkotzi",
+        },
+        {
+          url: "https://res.cloudinary.com/dkpywucqx/image/upload/v1698413831/YelpCamp/j7jo4khlnlckzacmqrzh.jpg",
+          filename: "YelpCamp/j7jo4khlnlckzacmqrzh",
+        },
+        {
+          url: "https://res.cloudinary.com/dkpywucqx/image/upload/v1698413831/YelpCamp/hc5g0yock1qz2kwhmfe7.jpg",
+          filename: "YelpCamp/hc5g0yock1qz2kwhmfe7",
+        },
+      ],
     });
     await camp.save();
   }
